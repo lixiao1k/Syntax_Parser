c581e91fd599b5ed2a94020ba2e5cfc34dbd60a8
李晓冬 <lixiao1k@163.com>
2017-11-14 21:30:03 +0800

    写完，但是没有用到词法分析，不满意，会改进

--- src/Syntax_Parser.java
+++ src/Syntax_Parser.java
@@ -3,9 +3,10 @@ import java.util.Stack;
 
 public class Syntax_Parser{
 
-    private String input_path = "input.txt";
-    private char[] input_char_list;
-    private Stack<Character> stack = new Stack<>();
+    private String input_path = "input.txt";//表达式输入文件
+
+    private char[] input_char_list;//存放输入表达式的字符
+    private Stack<Character> stack = new Stack<>();//文法符号序列栈
 
     private int[][] ppt =
             {      //+  ,  -  ,  *  ,  /  ,  (  ,  )  ,  i  ,  $
@@ -14,14 +15,16 @@ public class Syntax_Parser{
                     {1  ,  1  , -1  , -1  , -1  ,  2  , -1  ,  2},//C
                     {7  ,  7  ,  6  ,  6  , -1  ,  7  , -1  ,  7},//D
                     {-1 , -1  , -1  , -1  ,  0  , -1  ,  0  , -1},//E
-                    {-1 , -1  , -1  , -1  , 10  , -1  , 11  , -1}//F
-            };
+                    {-1 , -1  , -1  , -1  , 10  , -1  , 11  , -1},//F
+                    {-1 , -1  , -1  , -1  ,  5  , -1  ,  5  , -1}//T
+            };//LL(1)语法的预测分析表
+
     private String[] generations = {
-            "E->TC",
+            "E->GC",
             "C->AC",
             "C->ε",
-            "A->+T",
-            "A->-T",
+            "A->+G",
+            "A->-G",
             "T->FD",
             "D->BD",
             "D->ε",
@@ -29,7 +32,7 @@ public class Syntax_Parser{
             "B->/F",
             "F->(E)",
             "F->i"
-    };
+    };//维持产生式表
 
     private int getIndex(char ch){
         switch(ch){
@@ -39,6 +42,7 @@ public class Syntax_Parser{
             case 'D':return 3;
             case 'E':return 4;
             case 'F':return 5;
+            case 'G':return 6;
             case '+':return 0;
             case '-':return 1;
             case '*':return 2;
@@ -50,13 +54,13 @@ public class Syntax_Parser{
             default:return -1;
         }
     }
+
     public Syntax_Parser(){
         try {
             initData();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        print();
     }
 
     private boolean isTerminal(char ch){
@@ -68,6 +72,7 @@ public class Syntax_Parser{
     }
 
     private void initData() throws IOException {
+        //读入输入文件，初始化输入字符列表
         File file = new File(input_path);
         FileReader fileReader = new FileReader(file);
         BufferedReader bufferedReader = new BufferedReader(fileReader);
@@ -76,31 +81,60 @@ public class Syntax_Parser{
         while ((temp = bufferedReader.readLine())!=null){
             s+=temp;
         }
+        s =  s + '$';//输入字符串尾部加结束符
         input_char_list = s.toCharArray();
         bufferedReader.close();
-
+        //文法符号栈在初始化时栈底为'$'，在上一层为开始符'E'
         stack.push('$');
         stack.push('E');
     }
 
-    private void parser(){
+    public void parser(){
 
+        int in_ptr = 0;//标记输入位置的指针
+        char stack_head = stack.pop();//获取栈顶文法符号
+        char in_ch = input_char_list[in_ptr];//获取输入的终结符
+        boolean suceess = true;
+
+        while (!(in_ch=='$' && stack_head == '$')){
+            if(in_ch == stack_head){
+                System.out.println("匹配："+in_ch);
+                in_ch = input_char_list[++in_ptr];
+                stack_head = stack.pop();
+            }else if(isTerminal(stack_head)){
+                error();
+                suceess = false;
+                break;
+            }else if(ppt[getIndex(stack_head)][getIndex(in_ch)] == -1){
+                error();
+                suceess = false;
+                break;
+            }else if(ppt[getIndex(stack_head)][getIndex(in_ch)]!= -1){
+                String generation_string = generations[ppt[getIndex(stack_head)][getIndex(in_ch)]];
+                System.out.println(generation_string);
+                char[] temp = generation_string.toCharArray();
+                int length = temp.length;
+                for(int i = length-1;i>2;i--){
+                    if(temp[i]!='ε'){//如果时ε产生式，将产生式左部文法符弹出栈，即这里不做任何动作。
+                        stack.push(temp[i]);
+                    }
+                }
+                stack_head = stack.pop();
+            }
+        }
+        if(suceess){
+            System.out.println("解析成功！");
+        }else {
+            System.out.println("解析失败！");
+        }
     }
 
-
-    private void print(){
-        for(int i=0;i<input_char_list.length;i++){
-            System.out.print(input_char_list[i]);
-        }
+    private void error(){
+        System.out.println("error");
     }
 
     public static void main(String args[]){
         Syntax_Parser syntax_parser = new Syntax_Parser();
-
+        syntax_parser.parser();
     }
-
-
-
-
-
 }
